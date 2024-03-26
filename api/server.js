d3525f797f911a0cb2dedf5e32f429c2cc233a66
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-26 15:08:16 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+//bringing in express
+
+const recipesRouter = require('./recipes/recipes-router')
+//bringing in recipesRouter
 
 const server = express()
 //server instantiated by invoking express as a function 
 
 server.use(express.json())
 // teaching our server to parse json
+
+server.use('/api/recipes', recipesRouter)
+// the recipes router
+
+module.exports = server 
+//exposing it 
