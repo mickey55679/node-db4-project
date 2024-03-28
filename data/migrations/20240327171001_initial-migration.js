7752734f132bae0e0815e700fc68e67f5a0ab047
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-27 19:48:07 -0500

    wip

--- data/migrations/20240327171001_initial-migration.js
+++ data/migrations/20240327171001_initial-migration.js
@@ -2,14 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+
+//starting with recipes ad ingredients because they do not hae a foreign key.
+exports.up = async function(knex) {
+    await knex.schema
+      .createTable("recipes", table => {
+        table.increments()
+      })
+      .createTable("ingredients", table => {
+        table.increments();
+      })
+      .createTable("steps", table => {
+        table.increments();
+      })
+      .createTable("step_ingredients", table => {
+        table.increments();
+      });
   
 };
-
+// the order needs to be reversed to go down
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = async function(knex) {
+    await knex.schema
+    .dropTableIfExists('step_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
   
 };
