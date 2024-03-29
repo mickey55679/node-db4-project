fda645a9456a0295a013e8c327597c9edd17b8a0
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-28 19:07:43 -0500

    wip

--- data/migrations/20240327171001_initial-migration.js
+++ data/migrations/20240327171001_initial-migration.js
@@ -16,7 +16,16 @@ exports.up = async function(knex) {
         table.string('ingredients_unit', 50)
       })
       .createTable("steps", table => {
-        table.increments();
+        table.increments('step_id')
+        table.string('step_text', 200).notNullable()
+        table.integer('step_number').notNullable()
+        table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
       })
       .createTable("step_ingredients", table => {
         table.increments();
